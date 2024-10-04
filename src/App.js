596469f2046a4732d4d4584ec669fe1e7048d165
PRANETH <pranethcableguy@gmail.com>
2024-10-04 18:28:03 +0530

    ADDED LOGIN SCREEN UI AND LOGIN BACKGROUND

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Login/>
+      {/* <div className='org-name-container'>
+        <p>GHATKOPAR BILLAWAR ASSOCIATION, ASALPHA</p>
+      </div> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
